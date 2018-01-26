c1ccc4b98e42172633d40f93779fb5084089414c
Jasiu Leja <jleja09@gmail.com>
2018-01-26 18:50:11 -0500

    adds Gallery2: row wrap, .blue-tile flex-grow 2

--- client/components/index.js
+++ client/components/index.js
@@ -1,2 +1,3 @@
 export {default as Main} from './Main';
 export {default as Gallery1} from './Gallery1';
+export {default as Gallery2} from './Gallery2';
--- client/index.js
+++ client/index.js
@@ -1,11 +1,14 @@
 import './styling/index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Main, Gallery1} from './components';
+import {Main, Gallery1, Gallery2} from './components';
 
 ReactDOM.render(
   <Main>
     <Gallery1 />
+    <div className="separator" />
+    <Gallery2 />
+    <div className="separator" />
   </Main>,
   document.getElementById('app')
 );
